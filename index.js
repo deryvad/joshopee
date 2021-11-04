227238707435f4531e751d63f98983dedd423be6
Davy Red Navarro <davy.red.m.navarro@accenture.com>
2021-11-04 21:38:32 +0800

    asd

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 "use strict";
 const app = require("./app");
 
-const port = 5000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
 
 app.listen(port, function() {
   console.log('Listening to port ' + port);
